db6307ff4139cd69a3480660bd734e5f2a46fbe2
Marko Sipka <markoshipka@gmail.com>
2019-03-15 18:52:59 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,102 @@
+jQuery(document).on('ready', function ($) {
+    "use strict";
+
+    /*--------------------------
+        STICKY MAINMENU
+    ---------------------------*/
+    $("#mainmenu-area").sticky({
+        topSpacing: 0
+    });
+
+
+    /*---------------------------
+        SMOOTH SCROLL
+    -----------------------------*/
+    $('ul#nav li a[href^="#"], a.navbar-brand, a.scrolltotop').on('click', function (event) {
+        var id = $(this).attr("href");
+        var offset = 60;
+        var target = $(id).offset().top - offset;
+        $('html, body').animate({
+            scrollTop: target
+        }, 1500, "easeInOutExpo");
+        event.preventDefault();
+    });
+
+
+    /*----------------------------
+        MOBILE & DROPDOWN MENU
+    ------------------------------*/
+    jQuery('.stellarnav').stellarNav({
+        theme: 'dark'
+    });
+
+
+    /*----------------------------
+        SCROLL TO TOP
+    ------------------------------*/
+    $(window).scroll(function () {
+        var $totalHeight = $(window).scrollTop();
+        var $scrollToTop = $(".scrolltotop");
+        if ($totalHeight > 300) {
+            $(".scrolltotop").fadeIn();
+        } else {
+            $(".scrolltotop").fadeOut();
+        }
+
+        if ($totalHeight + $(window).height() === $(document).height()) {
+            $scrollToTop.css("bottom", "90px");
+        } else {
+            $scrollToTop.css("bottom", "20px");
+        }
+    });
+
+
+    /*--------------------------
+       PARALLAX BACKGROUND
+    ----------------------------*/
+    $(window).stellar({
+        responsive: true,
+        positionProperty: 'position',
+        horizontalScrolling: false
+    });
+
+    /*---------------------------
+        TESTMONIAL SLIDER
+    -----------------------------*/
+    var $testmonialCarousel = $('.testmonial-member-list');
+    $testmonialCarousel.owlCarousel({
+        merge: true,
+        smartSpeed: 1000,
+        loop: true,
+        nav: false,
+        center: true,
+        dots: true,
+        navText: ['<i class="fa fa-long-arrow-left"></i> Prev', 'Next <i class="fa fa-long-arrow-right"></i>'],
+        autoplay: true,
+        autoplayTimeout: 3000,
+        margin: 20,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            },
+            1200: {
+                items: 1
+            }
+        }
+    });
+
+    /*--------------------------
+        ACTIVE WOW JS
+    ----------------------------*/
+    new WOW().init();
+
+
+}(jQuery));
+
